ba6f0d52a5f06f881107779b071ffdbc6ff78800
Artemiy <birukovartemij@gmail.com>
2023-06-13 21:39:47 +0300

    Сделал задачу на сумму простых чисел в послед. N

---
+++ src/main/java/Homework2/Task1.java
@@ -0,0 +1,37 @@
+package Homework2;
+
+import java.util.Scanner;
+
+public class Task1 {
+    //Дана последовательность N целых чисел. Найти сумму простых чисел
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите длинну последовательности: ");
+        int n = scan.nextInt();
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            int num = scan.nextInt();
+            if (num > 1) {
+                if (naturalNumber(num) != false){
+                    sum += num;
+                }
+            }
+        }
+        System.out.println("sum = " + sum);
+
+    }
+
+    private static boolean naturalNumber(int n) {
+        boolean a = false;
+        int count = 0;
+        for (int j = 1; j <= n; j++) {
+            if (n > 2 && n % 2 == 0) break;
+            if (n % j == 0) {
+                count++;
+            }
+        }
+        if (count > 3) a = false;
+        if (count == 2) a = true;
+        return a;
+    }
+}
